45ec5809e04931a8ea135bf9165c2e9030f756ed
Eduardo Benitez <eduisic94@gmail.com>
2019-11-09 13:44:17 -0600

    iteration 2

--- iteracion1.js
+++ iteracion1.js
@@ -2,3 +2,12 @@ let hacker1 = "Denisse"
 console.log("The driver's name is " + hacker1)
 let hacker2 = "JUan"
 console.log("The navigator's name is " + hacker2)
+
+
+if(hacker1.length> hacker2.length)
+    console.log("the driver has the longest name, it has " + hacker1.length + " characters")
+else if(hacker2.length >hacker1.length)
+    console.log("it seems that the navigator has the longest name, it has " + hacker2.length + " characters")
+else
+console.log("wow, you both have equally longname, "+ hacker1.length+ " characteres")
+
